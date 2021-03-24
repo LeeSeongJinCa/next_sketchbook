fcb0d53bd27ae7736bd25206e2d6b6124967de17
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-03-24 19:58:07 +0900

    <update> default css, meta data, default page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,57 @@
-import Router from "next/router";
 import type { AppProps } from "next/app";
-import NProgress from "nprogress";
-import "nprogress/nprogress.css";
+import Router from "next/router";
+import Head from "next/head";
 
-import "../styles/global.css";
+import NProgress from "nprogress";
+
+import { css, Global } from "@emotion/react";
+import "nprogress/nprogress.css";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"
+        />
+      </Head>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+          }
+          html {
+            color: #212529;
+            cursor: default;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+              Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+              sans-serif;
+            font-size: 16px;
+            -moz-osx-font-smoothing: grayscale;
+            -webkit-font-smoothing: antialiased;
+          }
+          html,
+          body {
+            margin: 0;
+            padding: 0;
+          }
+          a {
+            color: #212529;
+            text-decoration: none;
+          }
+          a:hover {
+            text-decoration: underline;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 function HomePage() {
   return (
@@ -8,7 +9,12 @@ function HomePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Hello</h1>
+        <h1>
+          쓰레기<sub>는</sub> 쓰레기통<sub>에</sub>
+        </h1>
+        <Link href="/main">
+          <a>Main</a>
+        </Link>
       </main>
     </div>
   );
