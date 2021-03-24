d88ecaffe14c44f6474ebb632dedc7b9848427ee
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-03-24 19:52:44 +0900

    <update> : next.config.js -> next-image 연결

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-const path = require("path");
+const withImages = require("next-images");
 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-};
+module.exports = withImages();
