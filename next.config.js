71f43e3ae7f14b2d9807530cf58d3f56204b78a0
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-03-25 14:07:37 +0900

    update : baseUrl and assetPrefix -> "/"

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const withImages = require("next-images");
 module.exports = {};
 
 module.exports = withImages({
-  basePath: "/next_sketchbook",
-  assetPrefix: "/next_sketchbook",
+  basePath: "/",
+  assetPrefix: "/",
   exportPathMap: async function () {
     return {
       "/": { page: "/" },
