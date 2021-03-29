1514d748ed5275bdeb23567adb31975b4e6cc448
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-03-29 15:20:38 +0900

    add : navigation bar icon

---
+++ assets/index.ts
@@ -0,0 +1,8 @@
+export { default as homeSvg } from "./navigationBar/home.svg";
+export { default as RankSvg } from "./navigationBar/rank.svg";
+export { default as StatisticsSvg } from "./navigationBar/statistics.svg";
+export { default as homeBlueSvg } from "./navigationBar/homeBlue.svg";
+export { default as RankBlueSvg } from "./navigationBar/rankBlue.svg";
+export { default as StatisticsBlueSvg } from "./navigationBar/statisticsBlue.svg";
+
+export { default as trashCanSvg } from "./main/trashCan.svg";
