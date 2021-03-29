bc2892e636f2b4bb59565c1c5c8c888b85c25de1
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-03-29 15:23:24 +0900

    update : set default at main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,23 @@
-import Link from "next/link";
+import { GetStaticProps } from "next";
 
-function HomePage() {
-  return (
-    <div>
-      <main>
-        <h1>
-          쓰레기<sub>는</sub> 쓰레기통<sub>에</sub>
-        </h1>
-        <ul>
-          <li>
-            <Link href="/main">
-              <a>Main</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/statistics">
-              <a>Statistics</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/rank">
-              <a>Rank</a>
-            </Link>
-          </li>
-        </ul>
-      </main>
-    </div>
-  );
+import Main from "@components/main";
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { GOOGLE_MAP_API_KEY: apiKey } = process.env;
+
+  return {
+    props: {
+      apiKey,
+    },
+  };
+};
+
+interface Props {
+  apiKey: string;
+}
+
+function HomePage({ apiKey }: Props) {
+  return <Main apiKey={apiKey} />;
 }
 
 export default HomePage;
