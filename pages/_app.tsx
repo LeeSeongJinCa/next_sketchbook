3e491ae1054e3cd2ddb64bcff2e4b717359bafdd
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-03-29 15:23:42 +0900

    update : set mobile layout and reset css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,11 @@ import type { AppProps } from "next/app";
 import Router from "next/router";
 import Head from "next/head";
 import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 import { css, Global } from "@emotion/react";
-import "nprogress/nprogress.css";
+
+import MobileLayout from "layouts/MobileLayout";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -20,38 +22,82 @@ const App = ({ Component, pageProps }: AppProps) => {
         />
         <title>Google Map Search</title>
       </Head>
-      <Global
-        styles={css`
-          * {
-            box-sizing: border-box;
-          }
-          html {
-            color: #212529;
-            cursor: default;
-            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-              Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
-              sans-serif;
-            font-size: 16px;
-            -moz-osx-font-smoothing: grayscale;
-            -webkit-font-smoothing: antialiased;
-          }
-          html,
-          body {
-            margin: 0;
-            padding: 0;
-          }
-          a {
-            color: #212529;
-            text-decoration: none;
-          }
-          a:hover {
-            text-decoration: underline;
-          }
-        `}
-      />
-      <Component {...pageProps} />
+      <Global styles={globalStyle} />
+      <MobileLayout>
+        <Component {...pageProps} />
+      </MobileLayout>
     </>
   );
 };
+
+const globalStyle = css`
+  html,
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+  ol,
+  ul {
+    list-style: none;
+  }
+  blockquote,
+  q {
+    quotes: none;
+  }
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: "";
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  button {
+    cursor: pointer;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+  html {
+    color: #212529;
+    cursor: default;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-size: 16px;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+  }
+  a {
+    color: #212529;
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+`;
 
 export default App;
