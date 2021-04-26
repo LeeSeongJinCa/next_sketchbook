9b7c0cc9b536a12c7702d85df0a42f88c85385dd
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-26 11:46:07 +0900

    add : new google material icon and export assets

--- assets/index.ts
+++ assets/index.ts
@@ -1,9 +1,9 @@
-export { default as homeSvg } from "./navigationBar/home.svg";
-export { default as RankSvg } from "./navigationBar/rank.svg";
-export { default as StatisticsSvg } from "./navigationBar/statistics.svg";
-export { default as homeBlueSvg } from "./navigationBar/homeBlue.svg";
-export { default as RankBlueSvg } from "./navigationBar/rankBlue.svg";
-export { default as StatisticsBlueSvg } from "./navigationBar/statisticsBlue.svg";
+export { default as homeBlack } from "./navigationBar/home_black.svg";
+export { default as homeWhite } from "./navigationBar/home_white.svg";
+export { default as insightsBlack } from "./navigationBar/insights_black.svg";
+export { default as insightsWhite } from "./navigationBar/insights_white.svg";
+export { default as starBlack } from "./navigationBar/star_black.svg";
+export { default as starWhite } from "./navigationBar/star_white.svg";
 
 export { default as trashCanSvg } from "./main/trashCan.svg";
 export { default as paperWasteSvg } from "./main/paper-waste.png";
